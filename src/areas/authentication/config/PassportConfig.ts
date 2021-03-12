93d3c0254f48a7182038b70e121d7dcaa79eaa3f
Sean Luo <shawnluo415@gmail.com>
2021-03-12 14:10:47 -0800

    finished building pp class, but not tested

--- src/areas/authentication/config/PassportConfig.ts
+++ src/areas/authentication/config/PassportConfig.ts
@@ -2,5 +2,61 @@
 // TODO:                                 |
 //----------------------------------------
 // 🚀 Configure Passport.js Local Authentication in this file
+
+import { MockAuthenticationService } from "../services";
+
 //    Ensure code is fully typed wherever possible (unless inference can be made)
-export default class PassportConfig {}
+import passport from "passport";
+
+import * as passportLocal from 'passport-local';
+const LocalStrategy = passportLocal.Strategy;
+
+export default class PassportConfig {
+    private _passport;
+    private _strategy;
+
+
+    constructor(auth_service: MockAuthenticationService) {
+        this._passport = passport;
+        this._strategy = new LocalStrategy(
+            {
+              usernameField: "email",
+              passwordField: "password",
+            },
+            (email, password, done) => {
+              const user = auth_service.getUserByEmailAndPassword(email, password);
+              return user
+                ? done(null, user)
+                : done(null, false, {
+                    message: "Your login details are not valid. Please try again",
+                  });
+            }
+          );
+
+          this._passport.serializeUser(function (user, done) {
+            done(null, user.email);
+          });
+          
+          this._passport.deserializeUser(function (email, done) {
+            let user = auth_service.findUserByEmail(email);
+            if (user) {
+              done(null, user);
+            } else {
+              done({ message: "User not found" }, null);
+            }
+          });
+
+          this._strategy.use(this._strategy)
+    }
+
+    
+    public get passport() {
+        return this._passport;
+    }
+    
+    public get strategy() {
+        return this._strategy;
+    }
+
+}
+
