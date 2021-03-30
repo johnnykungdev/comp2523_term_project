94fddb4738e8ee0aa20cfc90893748ac7161af0e
Sean Luo <shawnluo415@gmail.com>
2021-03-30 00:03:52 -0700

    added recurseSelect

--- src/model/helpers/dbHelper.ts
+++ src/model/helpers/dbHelper.ts
@@ -3,7 +3,7 @@ import { database } from "../fakeDB";
 export class DbHelper {
   static readonly _db = database.users;
 
-  static select(conditions: { [key: string]: string }[]) {
+  static select(conditions: { [key: string]: string }[], db_level = this._db) {
     return conditions.reduce((state, condition) => {
       state = state.filter((user) => {
         if (user[Object.keys(condition)[0]] == Object.values(condition)[0]) {
@@ -13,10 +13,10 @@ export class DbHelper {
       });
 
       return state;
-    }, this._db);
+    }, db_level);
   }
 
-  static search(conditions: { [key: string]: string }[]) {
+  static search(conditions: { [key: string]: string }[], db_level = this._db) {
     return conditions.reduce((state, condition) => {
       state = state.filter((user) => {
         if (user[Object.keys(condition)[0]].includes(Object.values(condition)[0])) {
@@ -26,6 +26,25 @@ export class DbHelper {
       });
 
       return state;
-    }, this._db);
+    }, db_level);
+  }
+
+  // Example of array to pass to recurseSelect
+  // arr = [{ users: { username: "james123" } }, { posts: { id: "abc3" } }, { commentList: { id: "abc4" } }];
+
+  static recurseSelect(objs_arr: { [key: string]: { [key: string]: string } }[]) {
+    function recurse_loop(loopable, current) {
+      for (let i = 0; i < loopable[Object.keys(current)[0]].length; i++) {
+        if (
+          loopable[Object.keys(current)[0]][i][Object.keys(Object.values(current)[0])[0]] ==
+          Object.values(Object.values(current)[0])[0]
+        ) {
+          return loopable[Object.keys(current)[0]][i];
+        }
+      }
+      return null;
+    }
+
+    return objs_arr.reduce(recurse_loop, database);
   }
 }
