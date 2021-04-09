7f78ba0930ba9686435dad4f0d2b46a7d7010d22
Johnny Kung <kjohnathan.engi@gmail.com>
2021-04-09 13:58:59 -0700

    deleted firebase.ts

--- src/model/firebase.ts
+++
@@ -1,8 +0,0 @@
-// import * as admin from 'firebase-admin'
-
-// admin.initializeApp({
-//     credential: admin.credential.applicationDefault(),
-//     databaseURL: "https://comp2523-term-project-default-rtdb.firebaseio.com/"
-// })
-
-// export default 
