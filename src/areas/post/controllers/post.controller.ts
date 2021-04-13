b0e37c5759d9af332bf34e1c8d9d05071ebd1873
Sara Wasim <sarawasim@hotmail.com>
2021-04-12 17:04:52 -0700

    updated likePost feature

--- src/areas/post/controllers/post.controller.ts
+++ src/areas/post/controllers/post.controller.ts
@@ -3,11 +3,16 @@ import IController from "../../../interfaces/controller.interface";
 import IPostService from "../services/IPostService";
 import IUser from "../../../interfaces/user.interface";
 import IPost from '../../../interfaces/post.interface';
+import { database } from "../../../model/fakeDB";
+
+
 
 class PostController implements IController {
   public path = "/posts";
   public router = Router();
   private _postService: IPostService;
+  private _db = database.users;
+  private liked = []
 
   constructor(postService: IPostService) {
     this.initializeRoutes();
@@ -20,6 +25,8 @@ class PostController implements IController {
     this.router.post(`${this.path}/delete`, this.deletePost);
     this.router.post(`${this.path}/:id/comment`, this.createComment);
     this.router.post(`${this.path}`, this.createPost);
+    this.router.post(`${this.path}/:id/like`, this.likePost);
+
   }
 
   // 🚀 This method should use your postService and pull from your actual fakeDB, not the temporary posts object
@@ -43,6 +50,7 @@ class PostController implements IController {
 
   private createComment = async (req: Request, res: Response, next: NextFunction) => {
     const postId = req.params.id
+    console.log("postid******" + postId)
     console.log(req.body)
     
     function buildComment(userId: string, commentText: string): any {
@@ -74,6 +82,34 @@ class PostController implements IController {
     res.redirect("/posts")
   };
 
+  private getUserPost(userId: string, postId: string): object {
+    const user = this._db.find(user => user.id === userId)
+    return user.posts.find(post => post.id === postId )
+  }
+
+  private likePost = async (req: Request, res: Response, next: NextFunction) => {
+    const postId = req.params.id
+    const userId = req.body.userId
+
+    const userPost = this.getUserPost(userId, postId)
+    let likeCount = userPost['likes']
+    const idExists = this.liked.find(pId => pId === postId) === undefined ? false : true
+    if (!idExists) {
+      if (likeCount == undefined) {
+        likeCount = 0
+      }
+      userPost['likes'] = likeCount + 1
+      this.liked.push(postId)
+    } else {
+      if (likeCount != undefined && likeCount != 0) {
+
+        userPost['likes'] = likeCount - 1
+        const index = this.liked.indexOf(postId);
+        this.liked.splice(index, 1)
+      }
+    }
+    res.redirect("/posts")
+  };
 }
 
 export default PostController;
